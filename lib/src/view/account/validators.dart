21bbd9a0b1f7855dcedcf38e4baac12f7b20e6b2
David Ferland <david.ferland.swe@gmail.com>
2024-11-22 17:57:28 -0500

    validators

---
+++ lib/src/view/account/validators.dart
@@ -0,0 +1,32 @@
+String? validateEmail(String? value) {
+  if (value == null || value.isEmpty) {
+    return "The email is required.";
+  }
+
+  if (!RegExp(
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+  ).hasMatch(value)) {
+    return "The email is invalid.";
+  }
+
+  return null;
+}
+
+String? validatePassword(String? value) {
+  if (value == null || value.isEmpty) {
+    return "The password is required.";
+  }
+
+  if (value.length < 6) {
+    return "The password must be at least 6 characters.";
+  }
+
+  return null;
+}
+
+String? validateName(String? value) {
+  if (value!.isEmpty) {
+    return 'The first name is required.';
+  }
+  return null;
+}
