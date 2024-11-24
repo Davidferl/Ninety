e335b6268ce44b409b4dcd074829445275b1938b
Thomas Mondor <thomas.mondor.1@ulaval.ca>
2024-11-24 00:10:20 -0500

    fix: broken network image and routing fix

--- lib/src/view/addLog/tile_objective.dart
+++ lib/src/view/addLog/tile_objective.dart
@@ -10,28 +10,58 @@ class ObjectiveTile extends StatelessWidget {
   final String title;
   final String description;
 
-  const ObjectiveTile(
-      {super.key,
-      required this.imageUri,
-      required this.objectiveId,
-      required this.title,
-      required this.description});
+  const ObjectiveTile({
+    super.key,
+    required this.imageUri,
+    required this.objectiveId,
+    required this.title,
+    required this.description,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => context
-          .goNamed(Routes.progress.name, pathParameters: {"objectiveId": objectiveId}),
+      onTap: () {
+  final index = GoRouter.of(context).state?.pathParameters['index'];
+  context.goNamed(
+    Routes.progress.name,
+    pathParameters: {
+      "objectiveId": objectiveId,
+      "index": index!, 
+    },
+  );
+},
+
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(50.0),
-            child: Image.asset(
+            child: Image.network(
               imageUri,
               width: 50,
               height: 50,
               fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                }
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            (loadingProgress.expectedTotalBytes ?? 1)
+                        : null,
+                  ),
+                );
+              },
+              errorBuilder: (context, error, stackTrace) {
+                return const Icon(
+                  Icons.error,
+                  size: 50,
+                  color: Colors.red,
+                );
+              },
             ),
           ),
           horizontalSpace(10),
