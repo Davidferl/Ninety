11f5a720afec21eda9ff8987b0b953521f5802ca
Babouche <clnabergel@hotmail.fr>
2024-11-24 06:29:34 -0500

    Add internet to feed

--- lib/src/view/feed/feed.dart
+++ lib/src/view/feed/feed.dart
@@ -8,7 +8,9 @@ import 'package:bonne_reponse/src/group/domain/post_with_user_and_group.dart';
 import 'package:bonne_reponse/src/theme/colors.dart';
 import 'package:bonne_reponse/src/view/feed/group_pictures.dart';
 import 'package:bonne_reponse/src/view/feed/post_tile.dart';
+import 'package:bonne_reponse/src/view/widgets/no_internet.dart';
 import 'package:bonne_reponse/src/view/widgets/section_name.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -34,6 +36,18 @@ class Feed extends HookWidget {
     final groupData = useState<List<Group>>([]);
     final isPostsLoading = useState<bool>(true); // Track post loading status
     final isGroupsLoading = useState<bool>(true); // Track group loading status
+    final isConnectedToInternet = useState<bool>(false);
+
+    useEffect(() {
+      final connectivityStream =
+          Connectivity().onConnectivityChanged.listen((connectivityResult) {
+        isConnectedToInternet.value =
+            connectivityResult[0] == ConnectivityResult.mobile ||
+                connectivityResult[0] == ConnectivityResult.wifi;
+      });
+
+      return () => connectivityStream.cancel();
+    }, []);
 
     useEffect(() {
       Future<void> getPosts() async {
@@ -46,9 +60,11 @@ class Feed extends HookWidget {
         }
       }
 
-      getPosts();
+      if (isConnectedToInternet.value) {
+        getPosts();
+      }
       return () {};
-    }, []);
+    }, [isConnectedToInternet]);
 
     useEffect(() {
       Future<void> getGroups() async {
@@ -61,9 +77,11 @@ class Feed extends HookWidget {
         }
       }
 
-      getGroups();
+      if (isConnectedToInternet.value) {
+        getGroups();
+      }
       return () {};
-    }, []);
+    }, [isConnectedToInternet]);
 
     return SafeArea(
       child: Padding(
@@ -111,8 +129,7 @@ class Feed extends HookWidget {
                   ],
                 ),
               )
-            else
-              // Render group pictures when groups exist
+            else if (isConnectedToInternet.value)
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Padding(
@@ -144,23 +161,26 @@ class Feed extends HookWidget {
               ),
             verticalSpaceMedium,
             Expanded(
-              child: isPostsLoading.value
-                  ? const Center(child: CircularProgressIndicator())
-                  : postData.value.isEmpty
-                      ? Container()
-                      : ListView.builder(
-                          itemCount: postData.value.length, // Use actual length
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  0.0, 0.0, 0.0, 36.0),
-                              child: PostTile(
-                                postWithUserAndGroup: postData.value[index],
-                                userId: auth.user!.uid,
-                              ),
-                            );
-                          },
-                        ),
+              child: !isConnectedToInternet.value
+                  ? const NoInternet()
+                  : isPostsLoading.value
+                      ? const Center(child: CircularProgressIndicator())
+                      : postData.value.isEmpty
+                          ? Container()
+                          : ListView.builder(
+                              itemCount:
+                                  postData.value.length, // Use actual length
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      0.0, 0.0, 0.0, 36.0),
+                                  child: PostTile(
+                                    postWithUserAndGroup: postData.value[index],
+                                    userId: auth.user!.uid,
+                                  ),
+                                );
+                              },
+                            ),
             ),
           ],
         ),
